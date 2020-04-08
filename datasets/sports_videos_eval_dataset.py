ec1bb4c7870da99e7aa32a23616617ccd39a8e32
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-04-08 13:13:36 +0100

    Update sports_videos_eval_dataset.py

--- datasets/sports_videos_eval_dataset.py
+++ datasets/sports_videos_eval_dataset.py
@@ -5,10 +5,12 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import Normalize
 
+from utils.imutils import convert_bbox_centre_hw_to_corners
 import constants
 
+
 class SportsVideosEvalDataset(Dataset):
-    def __init__(self, dataset_path, img_wh):
+    def __init__(self, dataset_path, img_wh, bbox_scale_factor=1.2):
         super(SportsVideosEvalDataset, self).__init__()
 
         # Data
@@ -18,10 +20,13 @@ class SportsVideosEvalDataset(Dataset):
         self.vertices = data['vertices']
         self.body_shapes = data['shapes']
         self.genders = data['genders']
+        self.bbox_centres = data['centres']  # Tight bounding box centre
+        self.bbox_whs = data['whs']  # Tight bounding box width/height
 
         assert len(self.frame_paths) == len(self.vertices) == len(self.body_shapes) == len(self.genders)
 
         self.img_wh = img_wh
+        self.bbox_scale_factor = bbox_scale_factor
         self.normalize_img = Normalize(mean=constants.IMG_NORM_MEAN, std=constants.IMG_NORM_STD)
 
     def __len__(self):
@@ -31,9 +36,18 @@ class SportsVideosEvalDataset(Dataset):
         if torch.is_tensor(index):
             index = index.tolist()
 
-        # Inputs
+        # Input image - need to crop and resize because annoyingly, for sports videos dataset,
+        # the frames in the cropped_frames dir are not actually tightly cropped frames.
         frame_path = self.frame_paths[index]
         img = cv2.cvtColor(cv2.imread(frame_path), cv2.COLOR_BGR2RGB)
+        bbox_centre = self.bbox_centres[index]
+        bbox_wh = self.bbox_whs[index] * self.bbox_scale_factor
+        bbox_corners = convert_bbox_centre_hw_to_corners(bbox_centre, bbox_wh, bbox_wh)
+        top_left = bbox_corners[:2].astype(np.int16)
+        bottom_right = bbox_corners[2:].astype(np.int16)
+        top_left[top_left < 0] = 0
+        bottom_right[bottom_right < 0] = 0
+        img = img[top_left[0]: bottom_right[0], top_left[1]: bottom_right[1]]
         img = cv2.resize(img, (self.img_wh, self.img_wh), interpolation=cv2.INTER_LINEAR)
         img = np.transpose(img, [2, 0, 1])/255.0
 
